a5fa8323ca67ee0d344426f3e9da9f3e660f4a16
earthroot <earthro.ot46@yahoo.com>
2014-03-09 21:19:08 +0200

    fixed alpha

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :unset_cookie
+  #before_action :unset_cookie
   before_action :current_language
 
   def unset_cookie
