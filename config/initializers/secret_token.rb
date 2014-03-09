caa373c65873f8d4801639b5e37ec5b48a1aad21
earthroot <earthro.ot46@yahoo.com>
2014-03-09 14:01:11 +0200

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PollGenerator::Application.config.secret_key_base = '9f22dd7196a2393f3f62b70acc5f0907d24151f77648cfa59b557d4488a356867bd523b7881de169e2656985cb0632ab2156a90b098c72e1e4085e61b773ac03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PollGenerator::Application.config.session_store :cookie_store, key: '_PollGenerator_session'
